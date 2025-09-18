c4c0bb673a2bdfe5d802ed1ee771780fdc369ca6
blogmarinaluz <contato@educabrasil.fun>
2025-09-18 15:28:02 -0300

    Update page.tsx

--- src/app/entrar/[[...sign-in]]/page.tsx
+++ src/app/entrar/[[...sign-in]]/page.tsx
@@ -1,3 +1,11 @@
 // src/app/entrar/[[...sign-in]]/page.tsx
 import { SignIn } from "@clerk/nextjs";
-export default function Page() { return <SignIn />; }
+
+// Centraliza e adiciona respiro do topo e laterais (sem alterar o estilo do Clerk)
+export default function Page() {
+  return (
+    <div className="min-h-dvh w-full flex justify-center pt-8 sm:pt-12 px-4">
+      <SignIn />
+    </div>
+  );
+}
