02d7b278f309dff12738c6375bd99064663a6199
blogmarinaluz <contato@educabrasil.fun>
2025-09-18 13:36:57 -0300

    Update page.tsx

--- src/app/entrar/[[...sign-in]]/page.tsx
+++ src/app/entrar/[[...sign-in]]/page.tsx
@@ -1,13 +1,12 @@
 // src/app/entrar/[[...sign-in]]/page.tsx
 import { SignIn } from "@clerk/nextjs";
-import { ptBR } from "@clerk/localizations";
 import AuthShell from "@/components/auth/AuthShell";
 import { clerkAppearance } from "@/styles/clerkAppearance";
 
 export default function Page() {
   return (
     <AuthShell title="Entre na sua conta" subtitle="Acesse seus pedidos e preferências">
-      <SignIn appearance={clerkAppearance} localization={ptBR} signUpUrl="/cadastrar" />
+      <SignIn appearance={clerkAppearance} signUpUrl="/cadastrar" />
     </AuthShell>
   );
 }
